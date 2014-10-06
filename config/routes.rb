1090e0a9796326ada5ad20b4c7732492a32ed171
Gabe Berke-Williams <gabebw@gabebw.com>
2014-10-06 15:18:11 -0400

    Users can comment on images
    Errors are shown, too.

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   resources :galleries do
     resources :images, only: [:show, :new, :create, :edit, :update]
   end
+
+  resources :images, only: [] do
+    resources :comments, only: [:create]
+  end
 end
