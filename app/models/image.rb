b7cbb9723146a64d795a53846e89012942bfab37
Gabe Berke-Williams <gabebw@gabebw.com>
2014-10-08 15:43:41 -0400

    Show who liked an image

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,7 @@ class Image < ActiveRecord::Base
   belongs_to :gallery
   has_many :comments
   has_many :likes
+  has_many :likers, through: :likes, source: :user
 
   validates :name, presence: true
   validates :url, presence: true
